f6b6a84b62084e41e1d3126c5710b1085888e019
kBoris <khvanboris@gmail.com>
2019-10-07 12:35:19 -0700

    set up of translate app with context

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import UserCreate from './UserCreate'
+
+class App extends React.Component {
+  state = {language: 'english'};
+
+  onLanguageChange = language => {
+      this.setState ({language});
+    }
+
+  render () {
+    return (
+      <div className="ui container">
+        <div>
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange ('english')}
+          />
+          <i
+            className="flag in"
+            onClick={() => this.onLanguageChange ('hindi')}
+          />
+        </div>
+        <UserCreate />
+      </div>
+    )
+  }
+}
+
+export default App;
